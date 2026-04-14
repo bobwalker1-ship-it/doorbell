d6f512e7a61e38ff258ac6db01e2c546a2922308
bobwalker1-ship-it <bobwalker1@gmail.com>
2026-04-14 08:37:22 -0500

    Update monitor_doorbell_and_env.py
    misc edits

--- monitor_doorbell_and_env.py
+++ monitor_doorbell_and_env.py
@@ -5,8 +5,8 @@ import time
 import ujson
 from umqtt import *
 #-- Configuration ---
-WIFI_SSID = "coolhandbob"
-WIFI_PASS = "myboyblue599a"
+WIFI_SSID = ""
+WIFI_PASS = ""
 MQTT_CLIENTID = "BobsDoorBellMonitorEnv"
 MQTT_BROKER = "192.168.86.22" # Windows PC IP
 MQTT_TOPIC_ENV = "BobsDoorBell/env_data"
